09abf21f7e416337fe1af93221b318f169a3a06f
John Pearson <104274223+johnbp2@users.noreply.github.com>
2024-01-07 23:05:05 -0600

    ux changes

--- KeyBindingButlerFrameWork/Properties/Resources.Designer.cs
+++ KeyBindingButlerFrameWork/Properties/Resources.Designer.cs
@@ -71,6 +71,16 @@ namespace JohnBPearson.Windows.Forms.KeyBindingButler.Properties {
         }
         
         /// <summary>
+        ///   Looks up a localized resource of type System.Drawing.Bitmap.
+        /// </summary>
+        internal static System.Drawing.Bitmap butler {
+            get {
+                object obj = ResourceManager.GetObject("butler", resourceCulture);
+                return ((System.Drawing.Bitmap)(obj));
+            }
+        }
+        
+        /// <summary>
         ///   Looks up a localized resource of type System.Drawing.Icon similar to (Icon).
         /// </summary>
         internal static System.Drawing.Icon Icon1 {
